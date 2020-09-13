24f464207776761129a57c003e6dd1f3aa798949
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-09-13 06:11:37 +0300

    adding arf views

--- rems_backend/rems_api/views.py
+++ rems_backend/rems_api/views.py
@@ -1,8 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
-class HelloView(APIView):
+class HelloView(APIView, TokenAuthentication):
+    # permission_classes = (IsAuthenticated, )
+    keyword = "Bearer"
+
     def get(self, request):
+        print(request.user)
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+
+class ArfList(APIView):
+    pass
+
+
+class ArfDetail(APIView):
+    pass
