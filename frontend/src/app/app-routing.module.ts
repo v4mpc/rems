5fad7cee62e33462e30bdea8bb22a91093846ceb
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-09-23 12:37:51 +0300

    auth guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArfIndexComponent } from "./pages/arf-index/arf-index.component";
 import { ArfCreateComponent } from "./pages/arf-create/arf-create.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
-  {
-    path: 'arfs', component: ArfIndexComponent,
-  },
-
-  {
-    path: 'arfs-create', component: ArfCreateComponent,
-
-  }
+  { path: '', redirectTo: '/log-in', pathMatch: 'full' },
+  { path: 'log-in', component: LoginComponent },
+  { path: 'arfs', component: ArfIndexComponent, canActivate: [AuthGuard] },
+  { path: 'arfs-create', component: ArfCreateComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
