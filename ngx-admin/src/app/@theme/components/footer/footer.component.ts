ef3791343a2d92d6c24d02ed39becefbd3a7085c
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-11-21 00:29:45 +0300

    social links

--- ngx-admin/src/app/@theme/components/footer/footer.component.ts
+++ ngx-admin/src/app/@theme/components/footer/footer.component.ts
@@ -5,14 +5,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Created with ♥ by <b><a href="#" target="_blank">Yona Mahundi</a></b> 2019
+      Created with ♥ by <b><a href="#" target="_blank">Yona Mahundi</a></b> {{currentYear}}
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://github.com/v4mpc" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://www.linkedin.com/in/yona-mahundi-15a475a0/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
 export class FooterComponent {
+  currentYear = new Date().getFullYear()
 }
